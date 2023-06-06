f94a27904970d5b658fde3682965761b659fb72d
Abdul Majid <abdulmajidcse@gmail.com>
2023-06-06 20:48:48 +0600

    play with pinia

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,18 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+import { onMounted, onUpdated } from 'vue'
+
+const store = useCounterStore()
+
+onMounted(() => {
+  console.log(store.count)
+})
+
+onUpdated(() => {
+  console.log(store.count)
+})
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
-  
+import { useCounterStore } from '@/stores/counter'
+import { onMounted, onUpdated } from 'vue'
+
+const store = useCounterStore()
+
+onMounted(() => {
+  console.log(store.count)
+})
+
+onUpdated(() => {
+  console.log('Counter 2X: ', store.doubleCount)
+})
 </script>
 
 <template>
   <main>
     <h2>Home Page</h2>
+    <button @click="store.increment">++</button><span>{{ store.count }}</span
+    ><button @click="store.decrement">--</button>
   </main>
 </template>
