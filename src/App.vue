3c6848c86c21d464b3bdb906b40229448ab9ecee
Abdul Majid <abdulmajidcse@gmail.com>
2023-06-06 20:05:44 +0600

    vuejs init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <div class="main">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+header {
+  margin: 10px;
+  padding: 10px;
+}
+
+nav a {
+  padding: 10px;
+}
+
+div.main {
+  margin: 0 10px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+  
+</script>
+
+<template>
+  <main>
+    <h2>Home Page</h2>
+  </main>
+</template>
