3b38331a79cf34b0904280318d3598f36d27b828
Abdul Majid <abdulmajidcse@gmail.com>
2024-02-10 16:24:08 +0600

    practice event hadnler.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import { FwbButton } from 'flowbite-vue'
+
+const count = ref(0)
+
+const message = ref('I love VueJS!')
+
+function greeting(event, greet) {
+  alert(greet)
+  alert(message.value)
+  // `event` is the native DOM event
+  if (event) {
+    alert(event.target.tagName)
+  }
+}
+</script>
+
 <template>
   <div class="about">
-    <h1>Hello, about page</h1>
+    <h1 class="my-4">Hello, about page</h1>
+
+    <div class="grid grid-cols-2 gap-3">
+      <fwb-button gradient="blue" @click="count++">Click me - {{ count }}</fwb-button>
+
+      <fwb-button gradient="purple" @click="greeting($event, 'Welcome')">Open Message</fwb-button>
+    </div>
   </div>
 </template>
